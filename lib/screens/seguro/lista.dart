7812f6b2e6a51290efe66a1c69e3cb48f15ea345
André Vasconcelos <gp-andre.vasconcelos@uol.com.br>
2019-12-10 23:33:33 -0300

    Teste card

--- lib/screens/seguro/lista.dart
+++ lib/screens/seguro/lista.dart
@@ -1,6 +1,5 @@
 import 'package:banco_banco_vc/models/seguro/seguro.dart';
 import 'package:flutter/material.dart';
-import 'formulario.dart';
 
 const _tituloAppBar = "Seguros para você";
 
@@ -16,6 +15,7 @@ class ListaSeguro extends StatefulWidget {
 class ListaSeguroState extends State<ListaSeguro> {
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text(_tituloAppBar),
@@ -23,6 +23,7 @@ class ListaSeguroState extends State<ListaSeguro> {
       body: ListView.builder(
         itemCount: widget._seguros.length,
         itemBuilder: (context, indice) {
+          Navigator.pop(context, Seguro(100,200));
           final seguro = widget._seguros[indice];
           return ItemSeguro(seguro);
         },
@@ -53,11 +54,18 @@ class ItemSeguro extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        leading: Icon(Icons.monetization_on),
-        title: Text(_seguro.valor.toString()),
-        subtitle: Text(_seguro.codigo.toString()),
+    return Center(
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+             ListTile(
+              leading: Icon(Icons.album),
+              title: Text(this._seguro.descricao),
+               subtitle: Text("Algum outro texto??"),
+            )
+          ],
+        ),
       ),
     );
   }
